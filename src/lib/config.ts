34e41d44c1e9b265b01f621dd7f91721e5c8b5d9
do-dev26 <scrollbreaker83@gmail.com>
2026-05-11 21:48:16 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,13 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
 export const FIREBASE_CONFIG = {
-  apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyC3sIauDUSY-q7ZWx_n2EpIsctZsWGE6Bs",
+  authDomain: "data-credit-a48bd.firebaseapp.com",
+  projectId: "data-credit-a48bd",
+  storageBucket: "data-credit-a48bd.firebasestorage.app",
+  messagingSenderId: "154490671024",
+  appId: "1:154490671024:web:e87588a05df41c4c625075",
+  measurementId: "G-5E6DF2Y208"
+
 }
 export const PLANS = {
   free:       { label: 'Free',       words: 500,    price: 0  },
