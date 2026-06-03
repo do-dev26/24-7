bd7566791cd34457c8ebf676d1e94a3d2202341b
do-dev26 <scrollbreaker83@gmail.com>
2026-06-03 15:27:05 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,13 +1,13 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://24-7-hyaq.vercel.app/'
 
 export const FIREBASE_CONFIG = {
-  apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId:     process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey:            AIzaSyBsAEpp3hooCCHYEdmTNqTId9aWK0a69Ms
+  authDomain:        ai-vit-52666.firebaseapp.com
+  projectId:         ai-vit-52666
+  storageBucket:     ai-vit-52666.firebasestorage.app
+  messagingSenderId: 137839510307
+  appId:             1:137839510307:web:c8f56f623c8234b4638296"
+  measurementId:     G-CDTY9LJLC7
 }
 export const PLANS = {
   free:       { label: 'Free',       words: 500,    price: 0  },
