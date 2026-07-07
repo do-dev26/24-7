9fa09acb575c2480318627da763b0cf81c1d5883
do-dev26 <scrollbreaker83@gmail.com>
2026-07-08 01:25:47 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,14 +1,13 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://24-7-hyaq.vercel.app/'
-
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBsAEpp3hooCCHYEdmTNqTId9aWK0a69Ms",
-  authDomain: "ai-vit-52666.firebaseapp.com",
-  projectId: "ai-vit-52666",
-  storageBucket: "ai-vit-52666.firebasestorage.app",
-  messagingSenderId: "137839510307",
-  appId: "1:137839510307:web:c8f56f623c8234b4638296",
-  measurementId: "G-CDTY9LJLC7",
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyBKvn0N0hUzic1EWTJ0U9x9DO39bvEc4dc",
+  authDomain: "wsetw-7302c.firebaseapp.com",
+  projectId: "wsetw-7302c",
+  storageBucket: "wsetw-7302c.firebasestorage.app",
+  messagingSenderId: "916747800024",
+  appId: "1:916747800024:web:8b2baeb5b64194859276ad",
+  measurementId: "G-4MGLCB6L2B"
+};
 export const PLANS = {
   free:       { label: 'Free',       words: 500,    price: 0  },
   starter:    { label: 'Starter',    words: 5000,   price: 29 },
