701159c7ef46edcbeb255df8551a4334a3b92f26
do-dev26 <scrollbreaker83@gmail.com>
2026-07-08 15:34:44 +0300

    Delete src/lib/firebase.ts

--- src/lib/firebase.ts
+++
@@ -1,6 +0,0 @@
-import { initializeApp, getApps } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { firebaseConfig } from './config'
-const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export default app
