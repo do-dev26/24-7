78fe157f8dc4e3c8f1355e0b77e4c1885b7246b5
do-dev26 <scrollbreaker83@gmail.com>
2026-07-08 15:29:31 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { FIREBASE_CONFIG } from './config'
-
-const app = getApps().length ? getApps()[0] : initializeApp(FIREBASE_CONFIG)
+import { firebaseConfig } from './config'
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export default app
