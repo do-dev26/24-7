431e3b0a3f440a721548bbe14315e6fc5a9232c7
do-dev26 <scrollbreaker83@gmail.com>
2026-07-08 16:06:08 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,14 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://24-7-hyaq.vercel.app/'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCUeIklxbz6dq1vRdOveeZZl-9tdIhKuB4",
+  authDomain: "testora-98f88.firebaseapp.com",
+  projectId: "testora-98f88",
+  storageBucket: "testora-98f88.firebasestorage.app",
+  messagingSenderId: "243615465573",
+  appId: "1:243615465573:web:04af838f19fdf26140468c",
+  measurementId: "G-GG8LBD8HBB"
+};
 
 export const PLANS = {
   free:       { label: 'Free',       words: 500,    price: 0  },
